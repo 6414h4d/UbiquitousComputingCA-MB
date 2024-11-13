5541c2a83ef84018c1399742a77b814058a677d3
Eoghain-h4d <eoghain.lebioda.el@gmail.com>
2024-11-13 18:20:46 +0000

    added firebase initialisation

--- AndroidApp/app/src/main/java/com/example/microbitca/firebase_service.java
+++ AndroidApp/app/src/main/java/com/example/microbitca/firebase_service.java
@@ -3,9 +3,13 @@ package com.example.microbitca;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /*
 Add Firebase connectivity
@@ -14,13 +18,38 @@ If the connection fails, create a toast to alert the user
 
  */
 public class firebase_service extends Service {
-    FirebaseDatabase db = FirebaseDatabase.getInstance();
-    DatabaseReference dbRef = db.getReference("Message");
+    FirebaseDatabase db;
+    DatabaseReference dbRef;
 
+    public void onCreate(){
+        Toast.makeText(this, "Firebase Service started", Toast.LENGTH_SHORT).show();
+    }
 
+    public void onStart(Intent intent, int startid){}
+    public void onDestroy(){}
 
     public firebase_service() {
+
+        db = FirebaseDatabase.getInstance();
+        dbRef = db.getReference("somedata");
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Toast.makeText(getApplicationContext(),"somedata value is: " + value, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(firebase_service.this, "Firebase conn Cancelled", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 
     @Override
     public IBinder onBind(Intent intent) {
